c3dd6dca147191966307da4a90a790d3e27b49cf
Luis Rovirosa Mairlot <luis@codium.team>
2024-04-04 18:58:15 +0200

    refactor: use reflect to assert not equals

--- go/simon_test.go
+++ go/simon_test.go
@@ -1,6 +1,7 @@
 package main_test
 
 import (
+	"reflect"
 	"testing"
 
 	. "github.com/JesusValera/simon-says-kata"
@@ -18,28 +19,11 @@ func TestWIPGame(t *testing.T) {
 }
 */
 
-func areEqual(slice1, slice2 []string) bool {
-	// If the lengths of the slices are different, they can't be equal
-	if len(slice1) != len(slice2) {
-		return false
-	}
-
-	// Iterate over each element of the slices and compare them
-	for i := range slice1 {
-		if slice1[i] != slice2[i] {
-			return false
-		}
-	}
-
-	// If all elements are equal, return true
-	return true
-}
-
 func TestColorsAfterPlay(t *testing.T) {
 	game := Game{}
 	colors := game.Play()
 
-	if !areEqual(colors, []string{"yellow"}) {
+	if !reflect.DeepEqual(colors, []string{"yellow"}) {
 		t.Error("error")
 	}
 }
